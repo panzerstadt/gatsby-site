f7f4532781462e6064ef2adc72fba351bb6503ff
Matthew Yong <37174946+matt-writes-code@users.noreply.github.com>
2019-04-01 21:41:59 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
   <p>I'm on Netlify!</p>
+  <p>I'm a new branch.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
